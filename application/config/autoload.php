cb7836cb70d721f6db63c0499aff6a18fb95e223
Tavilo Breno <breno_wk2@hotmail.com>
2024-06-15 19:48:52 -0300

    Traduzindo o Codeigniter 3

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -117,7 +117,8 @@ $autoload['config'] = array();
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array();
+$config['language'] = array('portuguese-brazilian');
+
 
 /*
 | -------------------------------------------------------------------
