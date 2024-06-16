5698edf70e5d741115d8d8939fe74c46886ce834
Tavilo Breno <breno_wk2@hotmail.com>
2024-06-16 12:16:01 -0300

    Listando formas de pagamento e traduzindo datatables

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -41,6 +41,9 @@
                                 <div class="nav-item">
                                     <a data-toggle="tooltip" data-placement="top" title="Precificações" href="<?php echo base_url('precificacoes'); ?>"><i class="ik ik-dollar-sign"></i><span>Precificações</span></a>
                                 </div>
+								<div class="nav-item">
+                                    <a data-toggle="tooltip" data-placement="top" title="Formas de Pagamento" href="<?php echo base_url('formas'); ?>"><i class="fas fa-comment-dollar"></i><span>Formas de Pagamento</span></a>
+                                </div>
                             </nav>
                         </div>
                     </div>
